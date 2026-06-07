70f99bb68a1aff2d0ac56501804fcfea9c72f15a
kocaburakefe-ops <kocaburakefe@gmail.com>
2026-06-07 16:49:24 +0300

    Change runtime version from V2.2 to V3.0
    Updated version number in the program header.

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from motor_cekirdegi import DeterCodeMotoru
 
 def sistemi_ateşle():
-    # Motoru başlat
     motor = DeterCodeMotoru()
-    
     print("==================================================")
-    # Burak ismi statik kalsın diye ekledik reis
-    print("      DETERCODE PROGRAMLAMA DİLİ - RUNTIME V2.2   ")
+    print("      DETERCODE PROGRAMLAMA DİLİ - RUNTIME V3.0   ")
     print("==================================================")
-    
-    # Ana senaryo dosyasını çalıştır
     motor.dosya_calistir("senaryolar/saf_guc.deter")
 
 if __name__ == "__main__":
