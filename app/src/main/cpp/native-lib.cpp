d489e187d4f713a0d5c163e36869395073d55170
kocaburakefe-ops <kocaburakefe@gmail.com>
2026-06-09 21:55:55 +0300

    Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,8 +6,7 @@
 
 // Arka planda (asenkron) yakıt pompalayan gizli fonksiyon
 std::string asyncFuelPump() {
-    // Ağır bir veri işleme sürecini simüle ediyoruz (Örn: Buluttan veri çekme veya analiz)
-    // Ana arayüzü kitlememek için arka planda 2 saniye piston devredecek
+    // Ağır bir veri işleme sürecini simüle ediyoruz (Arayüzü kitlememek için arkada 2 saniye piston devredecek)
     std::this_thread::sleep_for(std::chrono::seconds(2));
     return "Asenkron Yakıt Pompası: Veri Depodan Çekildi ve Motor Ateşlendi! ⚡";
 }
@@ -16,17 +15,17 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_kocaburakefe_detercode_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+    // 1. Paket: İşlemciden gelen saf performans mesajı
     std::string nativeMessage = "C++ Hibrit Motoru Aktif: Saf Performansla Calisiyor! 🚀";
     return env->NewStringUTF(nativeMessage.c_str());
 }
 
-// YENİ ENJEKSİYON: Java tarafına asenkron veriyi fırlatan köprü
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_kocaburakefe_detercode_MainActivity_getAsyncData(
         JNIEnv* env,
         jobject /* this */) {
     
-    // C++ thread mekanizması ile fonksiyonu arka planda ateşliyoruz
+    // 2. Paket: C++ thread mekanizması ile fonksiyonu arka planda ateşliyoruz
     auto futureResult = std::async(std::launch::async, asyncFuelPump);
     
     // Veri hazır olduğunda yakalayıp Java'ya teslim ediyoruz
