87bb24bb2df412de62787464a09597b9b1941bd0
kocaburakefe-ops <kocaburakefe@gmail.com>
2026-06-10 09:47:06 +0300

    Create core_unclogger.h

---
+++ app/src/main/cpp/feature_1_unclogger/core_unclogger.h
@@ -0,0 +1,8 @@
+#ifndef CORE_UNCLOGGER_H
+#define CORE_UNCLOGGER_H
+
+// Ana motorun (native-lib.cpp) bu odayı tanıması için vize pasaportu usta
+extern "C" void start_core_unclogging();
+
+#endif // CORE_UNCLOGGER_H
+
