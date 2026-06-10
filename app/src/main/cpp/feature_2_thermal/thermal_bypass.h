7e3b8cd87ab326fcd439a2293003da1934362ff5
kocaburakefe-ops <kocaburakefe@gmail.com>
2026-06-10 09:54:58 +0300

    Create thermal_bypass.h

---
+++ app/src/main/cpp/feature_2_thermal/thermal_bypass.h
@@ -0,0 +1,8 @@
+#ifndef THERMAL_BYPASS_H
+#define THERMAL_BYPASS_H
+
+// Ana motorun (native-lib.cpp) bu odayı tetiklemesi için gereken ana şalter
+extern "C" void apply_thermal_priority_bypass();
+
+#endif // THERMAL_BYPASS_H
+
