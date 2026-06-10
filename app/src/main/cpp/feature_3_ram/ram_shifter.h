159d815e25d8b62fb5312059d1766b24b005c660
kocaburakefe-ops <kocaburakefe@gmail.com>
2026-06-10 10:04:47 +0300

    Create ram_shifter.h with execute_ram_flush declaration
    Add header file for RAM shifter functionality.

---
+++ app/src/main/cpp/feature_3_ram/ram_shifter.h
@@ -0,0 +1,7 @@
+#ifndef RAM_SHIFTER_H
+#define RAM_SHIFTER_H
+
+// Ana motorun (native-lib.cpp) bu odayı tetiklemesi için gereken ana şalter
+extern "C" void execute_ram_flush();
+
+#endif // RAM_SHIFTER_H
