8bca765e1614e0f3499ef45858d3bde0beaaece4
kocaburakefe-ops <kocaburakefe@gmail.com>
2026-06-10 10:51:01 +0300

    Create asm_neon_booster.h for NEON assembly
    Add header file for NEON assembly function declaration.

---
+++ app/src/main/cpp/asm_neon_booster.h
@@ -0,0 +1,8 @@
+#ifndef ASM_NEON_BOOSTER_H
+#define ASM_NEON_BOOSTER_H
+
+// Ana motorun (native-lib.cpp) bu saf Assembly odasını tetiklemesi için şalter
+extern "C" void execute_neon_assembly_vector();
+
+#endif // ASM_NEON_BOOSTER_H
+
