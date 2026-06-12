b5751c2624c65e27e3dd8216c0e8fec53c13d113
kocaburakefe-ops <kocaburakefe@gmail.com>
2026-06-12 21:24:12 +0300

    Add CodeVirtualizer class for code virtualization

---
+++ Core/AntiTamper/code_virtualizer.h
@@ -0,0 +1,13 @@
+#ifndef CODE_VIRTUALIZER_H
+#define CODE_VIRTUALIZER_H
+#include <iostream>
+
+class CodeVirtualizer {
+public:
+    CodeVirtualizer() { std::cout << "[🛡️ ANTITAMPER]: Code_Virtualizer devrede. Siber muhur basıldı." << std::endl; }
+    void virtualize_critical_blocks() {
+        std::cout << "[🔐 VIRTUALIZE]: Kritik kodlar ozel sanal makine diline cevrildi. Tersine muhendislik imkansiz." << std::endl;
+    }
+};
+#endif
+
