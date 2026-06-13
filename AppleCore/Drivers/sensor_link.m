ff41d62c68feaa5385ff8afe2b7fe110e5286b5b
kocaburakefe-ops <kocaburakefe@gmail.com>
2026-06-13 18:37:34 +0300

    Add sensor_link.m to link sensor data

---
+++ AppleCore/Drivers/sensor_link.m
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+void linkSensors() {
+    // Sensör verilerini DeterCode'a ham veri olarak aktar
+    NSLog(@"[DeterCode] Sensor_Link: Veri yolu oluşturuldu.");
+}
+
