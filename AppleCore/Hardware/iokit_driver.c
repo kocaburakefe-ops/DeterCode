c4b6e9fcf3c91e4f321f424748c6f80ec523aba9
kocaburakefe-ops <kocaburakefe@gmail.com>
2026-06-13 21:16:52 +0300

    Add IOKit service connection function

---
+++ AppleCore/Hardware/iokit_driver.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void connect_to_iokit_service() {
+    // Apple IOKit master port simülasyonu
+    printf("[DeterCode::IOKit] IOService master port açılıyor...\n");
+    printf("[DeterCode::IOKit] Donanım kayıtlarına doğrudan kanal tanımlandı.\n");
+}
+
